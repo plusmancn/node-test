ecb1c5b33844811a16859f8873dfcdde1e17474a
Your Name <you@example.com>
2015-07-14 09:36:08 +0800

    实现1级链式调用

--- 03-promise-a/case1.js
+++ 03-promise-a/case1.js
@@ -1,13 +1,85 @@
 'use strict';
 /**
- * node 简易promise实现
+ * node 简易私有Promise/A实现，实现链式调用
  */
+var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
-var Promise = function(){
+/**
+ * @description promise 实现
+ */
+var _Promise = function(){
   EventEmitter.call(this);
+};
+
+util.inherits(_Promise,EventEmitter);
+
+_Promise.prototype.then = function(fulfillHandler,errorHandler,processHandler) {
+  if (typeof fulfillHandler === 'function') {
+    this.once('success',fulfillHandler);
+  }
+
+  if( typeof errorHandler === 'function'){
+    this.once('error',errorHandler);
+  }
+
+  if (typeof processHandler === 'function') {
+    this.on('process',processHandler);
+  }
+
+  return this; // 实现链式调用
+};
+
+
+/**
+ * @description derfered 实现
+ * 这里违背了一个原则，未完成态只能转向成功态和失败态，且过程不可逆
+ * 可以利用state实现这种状态机制
+ */
+var _Deferred = function(){
+  this.state = 'unfinished';
+  this._promsie = new _Promise();
+};
+
+_Deferred.prototype.resolve = function(result) {
+  this.state = 'fulfill';
+  this._promsie.emit('success',result);
+};
+
+_Deferred.prototype.reject = function(error) {
+  this.state = 'error';
+  this._promsie.emit('error',error);
+};
+
+_Deferred.prototype.process = function(data) {
+  this.state = 'process';
+  this._promsie.emit('process',data);
+};
+
+
+/**
+ * @description 1级链式调用
+ */
+function startFunc () {
+  var _deferred =  new _Deferred();
+
+  setTimeout(_deferred.resolve.bind(_deferred),1000,'startFunc resolve');
+  setTimeout(_deferred.reject.bind(_deferred),2000,'startFunc reject');
+  var intervalId = setInterval(_deferred.process.bind(_deferred),500,'startFunc process' + Date.now()); // 记录定时器ID
+  setTimeout(clearInterval,3000,intervalId); // 4 秒后清空定时器 
+
+  return _deferred._promsie;
 }
 
-Promise.prototype.then = function(fulfil) {
-  // body...
-};
+
+startFunc()
+.then(function(result){
+  console.log(result); // startFunc resolve
+},function(error){
+  console.log(error); // startFunc reject
+},function(process){
+  console.log(process); // startFunc process1436803901861
+}).then();
+
+
+
--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # node-test
-node playground
+>node playground
+大量代码出自 [JackSon Tian](https://github.com/JacksonTian) 所著《深入浅出node.js》
 
 ## Index
-* 02-function-scope   函数作用域和this指针测试
-* 05-nginx-with-node  nginx下node反向代理测试
-* 06-memcached-usage  结合npm memcached 使用方法测试
+```
+* 02-function-scope     函数作用域和this指针测试
+* 05-nginx-with-node    nginx下node反向代理测试
+* 06-memcached-usage    结合npm memcached 使用方法测试
 * 07-deploy-bash-shell  部署脚本测试
+* 08-env-detech         服务器端&浏览器端运行环境检测类测试        
+```
