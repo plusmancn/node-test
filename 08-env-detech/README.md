f451fba3aa4ca185bf74f7386dcee554253da446
plusmancn <plusmancn@gmail.com>
2015-07-24 20:41:01 +0800

    修正浏览器环境下环境检测类挂载对象错误问题。

--- 08-env-detech/README.md
+++ 08-env-detech/README.md
@@ -1,2 +1,8 @@
 # 浏览器环境检测类
 >兼容AMD，CMD，CommonJS，以及全局挂载
+
+## Usage
+**浏览器环境**
+```javascript
+var probe = new browserEncDetech();
+```
--- 08-env-detech/browser_envdetect.js
+++ 08-env-detech/browser_envdetect.js
@@ -11,7 +11,7 @@
     module.exports = defination();
   }else{
     // 浏览器支持
-    this[name] = defination(); 
+    window[name] = defination(); 
   }
 })('browserEnvdetect',function(){
   /**
