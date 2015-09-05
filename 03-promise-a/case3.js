04ace6064db0920b45774f96c4d953c4ca70aa4f
plusmancn <plusmancn@gmail.com>
2015-09-05 21:56:01 +0800

    链式调用PromiseA

--- 03-promise-a/case3.js
+++ 03-promise-a/case3.js
@@ -1,3 +1,6 @@
+/**
+ * 延迟部分应该独立，健壮性能一般
+ */
 'use strict';
 var _Promise = function(_derfer){
   this.resolveStack = [];
@@ -12,7 +15,16 @@ _Promise.prototype.then = function(handleFulFill,handleReject){
 }
 
 _Promise.prototype.resolve = function(success){
-  this.resolveStack.shift()(success);
+  var resolveHandle = this.resolveStack.shift();
+  var res = resolveHandle(success);
+  if (res) {
+    var that = this;
+    res.then(function(inSuccess){
+      that.resolve(success);
+    },function(inError){
+      that.resolve(inError);
+    });
+  };
   this.rejectStack.shift();
 }
 
@@ -29,11 +41,17 @@ _Promise.prototype.reject = function(error){
 var p = new _Promise(function(resolve,reject){
   setTimeout(function(){
     Math.random() > 0.5 ? resolve('success') : reject('fail');
-  })
+  },0)
 });
 
 p.then(function(success){
   console.log(success);
+  var pIn = new _Promise(function(resolve,reject){
+    setTimeout(function(){
+      Math.random() > 0.5 ? resolve('pin success') : reject('pin fail');
+    },1000);
+  });
+  return pIn;
 }).then(function(success){
   console.log('then2',success);
 },function(error){
